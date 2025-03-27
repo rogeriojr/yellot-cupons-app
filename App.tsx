58bd75b7829f05ede04afe15bfa7b07204a57e9e
rogeriojr <rogeriojr1100@gmail.com>
2025-03-27 13:39:52 -0300

    lint: remove no use imports

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 // Navegação
 import AppNavigator from "./src/navigation/AppNavigator";
-
 // Contextos
 import { AuthProvider } from "./src/contexts/AuthContext";
 import { ThemeProvider } from "./src/contexts/ThemeContext";
-
-// Types
-import { RootTabParamList } from "./src/types/navigation";
-
-const Tab = createBottomTabNavigator<RootTabParamList>();
 
 export default function App() {
   return (
